0f33f4386af09a966133fc77a88f09966de6cb67
LBernabei <crysis91@hotmail.it>
2016-01-13 12:56:17 +0100

    Correzione CSS e progressi Login
    Ho corretto qualcosa sul CSS, tipo il tablet era impostato come lo
    smartphone con le colonne da 12, invece doveva rimanere 9 e 3 come il
    desktop.
    Ho poi continuato il login, dovrei aver ultimato lo script con tutti gli
    errori ecc e ho cambiato dinamicamente la pagina mettendo anche il
    logout ('ciao, user' era già presente).
    NB Bisogna ricordarsi di mettere uno script a parte per i film
    (loginFilm.js) perchè cambia il percorso del file php da prendere!

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-function registra(e) {
+function login(e) {
     
     var form = document.getElementById('login_form');
     if (form.checkValidity() == true) {   /*viene effettuato un check del form prima del run dello script*/
@@ -7,47 +7,59 @@ function registra(e) {
             var div;
             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                 if(xmlhttp.responseText == "OK") {
-                    div = document.getElementById('signup');
+                    div = document.getElementById('signup'); /* modifico #signup */
+                    div.removeAttribute('href');
                     div.innerHTML = 'Ciao, NOME-UTENTE!';
+                    div.style.cursor = 'pointer';
+                    div = document.getElementById('login'); /* modifico #login */
+                    div.innerHTML = '<a><img src="img/logout.png">Logout</a>';
+                    div.removeAttribute('onmouseover');
+                    div.removeAttribute('onmouseout');
+                    div.setAttribute("onclick", "logout(event)");
                 }
                 else if(xmlhttp.responseText == "USER_ERROR") {
-                    div = document.getElementById('ERROR');
-                    div.innerHTML = 'Errore: username già esistente!';
+                    div = document.getElementById('ERROR2');
+                    div.innerHTML = 'Username errato!';
                     div.style.color = "red";
                     div.style.textDecoration = "underline";
-                    div.style.padding = "5px 15px";
+                    div.style.padding = "10px 5px 0px 5px";
+                    div.style.textTransform = "none";
+                    div.style.fontWeight = "normal";
                 }
-                else if(xmlhttp.responseText == "MAIL_ERROR") {
-                    div = document.getElementById('ERROR');
-                    div.innerHTML = 'Errore: esiste già un account registrato con quella email!';
+                else if(xmlhttp.responseText == "PWD_ERROR") {
+                    div = document.getElementById('ERROR2');
+                    div.innerHTML = 'Password errata!';
                     div.style.color = "red";
                     div.style.textDecoration = "underline";
-                    div.style.padding = "5px 15px";
+                    div.style.padding = "10px 5px 0px 5px";
+                    div.style.textTransform = "none";
+                    div.style.fontWeight = "normal";
                 }
                 else if(xmlhttp.responseText == "DB_ERROR") {
-                    div = document.getElementById('ERROR');
-                    div.innerHTML = 'Errore: non è stato possibile effettuare la registrazione, riprova più tardi.';
+                    div = document.getElementById('ERROR2');
+                    div.innerHTML = 'Login non disponibile.<br>Riprova più tardi.';
                     div.style.color = "red";
                     div.style.textDecoration = "underline";
-                    div.style.padding = "5px 15px";
+                    div.style.padding = "10px 5px 0px 5px";
+                    div.style.textTransform = "none";
+                    div.style.fontWeight = "normal";
                 }
                 else { /*errore generico in caso dal php venga passata una stringa non riconosciuta*/
-                    div = document.getElementById('ERROR');
+                    div = document.getElementById('ERROR2');
                     div.innerHTML = 'Errore.';
                     div.style.color = "red";
                     div.style.textDecoration = "underline";
-                    div.style.padding = "5px 15px";
+                    div.style.padding = "10px 5px 0px 5px";
+                    div.style.textTransform = "none";
+                    div.style.fontWeight = "normal";
                 }
             }
         };
         
-        var name = document.getElementById('name').value;
-        var surname = document.getElementById('surname').value;
-        var mail = document.getElementById('mail').value;
-        var utente = document.getElementById('utente').value;
-        var pswd = document.getElementById('pswd').value;
+        var user = document.getElementById('user').value;
+        var pwd = document.getElementById('pwd').value;
         
-        var params = "name=" + name + "&surname=" + surname + "&mail=" + mail + "&utente=" + utente + "&pswd=" + pswd;
+        var params = "user=" + user + "&pwd=" + pwd;
         
         xmlhttp.open("GET", "php/login_prova.php?" + params, true);
         xmlhttp.send();
